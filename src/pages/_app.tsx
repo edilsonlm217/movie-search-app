ef694ab4d3545eca2b7b11ba758478b2158254a6
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-24 01:07:55 -0400

    Refactor layout in _app.tsx for improved structure and readability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FlexBox, FlexBoxDirection } from '@ui5/webcomponents-react'
+
+import styles from '../styles/_app.module.scss'
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <FlexBox 
+      direction={FlexBoxDirection.Column} 
+      className={styles.appContainer}
+    >
+      
+      <FlexBox className={styles.headerContainer}>
+        <Header />
+      </FlexBox>
+      
+      <FlexBox direction={FlexBoxDirection.Column} className={styles.contentContainer}>
+        <Component {...pageProps} />
+      </FlexBox>
+      
+      <FlexBox className={styles.footerContainer}>
+        <p>footer-component goes here</p>
+      </FlexBox>
+
+    </FlexBox>
+  );
 }
