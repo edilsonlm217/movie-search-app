bd2284662ac908ef9f5c5b4558111d67bc3b99e6
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-23 22:33:11 -0400

    Subject: Add UI5 web components and React components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@ui5/webcomponents-react';
 
 export default function Home() {
   return (
     <>
       <p>home-page works</p>
+      <Button>Click me!</Button>
     </>
   )
 }
