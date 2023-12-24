014c66eb740d09f1cf10a1b330432c5989e1b9e7
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-23 22:26:09 -0400

    Refactor next.config.js to include sassOptions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
-
-module.exports = nextConfig
