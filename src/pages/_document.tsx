970da511c8f14d88fab01b05202ce089e17f3f71
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-23 22:42:15 -0400

    Add external font link to Head component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Link rel="stylesheet" href="https://fonts.googleapis.com/css?family=SAP+72" />
+      </Head>
       <body>
         <Main />
         <NextScript />
