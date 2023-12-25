0457decacc7223bcd01b4c8288a624af3436e7de
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-24 21:46:14 -0400

    Refactor page layout to include Footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { FlexBox, FlexBoxDirection } from '@ui5/webcomponents-react'
-import Header from '../components/Header';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 import styles from '../styles/_app.module.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FlexBox direction={FlexBoxDirection.Column} className={styles.appContainer}>
-      
-      <Header />
+    <FlexBox 
+      id="app-container" 
+      className={styles.appContainer} 
+      direction={FlexBoxDirection.Column}
+    >
+      <Header id="app-header" />
       
       <FlexBox 
-        direction={FlexBoxDirection.Column} 
+        id="app-content" 
         className={styles.contentContainer}
+        direction={FlexBoxDirection.Column} 
       >
         <Component {...pageProps} />
       </FlexBox>
       
-      <FlexBox className={styles.footerContainer}>
-        <p>footer-component goes here</p>
-      </FlexBox>
-
+      <Footer id="app-footer" />
     </FlexBox>
   );
 }
