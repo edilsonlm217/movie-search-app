f0b292ca1d010607013e82fae562ac6960b0f661
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-26 15:10:45 -0400

    Refactor home page to use UI5 web components for improved styling and theming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import { Title, Text, ThemeProvider } from '@ui5/webcomponents-react'
+
 import styles from '../styles/index.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.pageContainer}>
-      <div className={styles.content}>
-        <p>home-page works</p>
-        <p>home-page works</p>
+    <ThemeProvider>
+      <div className={styles.pageContainer}>
+        <div className={styles.content}>
+          <Title id="page-title" level="H3" className={styles.pageTitle}>
+            Title
+          </Title>
+          
+          <Text id="page-description" className={styles.pageDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Laborum nobis itaque deserunt ipsa voluptate inventore minima voluptas voluptatibus tempora sed?
+          </Text>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
