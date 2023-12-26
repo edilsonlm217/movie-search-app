e6d0216a5108feb9a24d3e2df14cb08ab88372d2
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-26 15:48:17 -0400

    Create MovieDetail component and styles
    Add new MovieDetail component with associated styles to enhance
    organization and reusability of movie detail display.

---
+++ src/components/MovieDetail/index.tsx
@@ -0,0 +1,13 @@
+
+
+import styles from './movie-detail.module.scss'
+
+interface MovieDetailProps {}
+
+const MovieDetail: React.FC<MovieDetailProps> = () => {
+  return (
+    <p>movie-detail component works</p>
+  )
+}
+
+export default MovieDetail;
