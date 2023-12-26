3994deb933833e00648ee3421c1001e2030e5d79
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-26 18:51:10 -0400

    Refactor _app.tsx to consistently use double quotes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { FlexBox, FlexBoxDirection } from '@ui5/webcomponents-react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { FlexBox, FlexBoxDirection } from "@ui5/webcomponents-react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-import styles from '../styles/_app.module.scss'
+import styles from "../styles/_app.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FlexBox 
-      id="app-container" 
-      className={styles.appContainer} 
+    <FlexBox
+      id="app-container"
+      className={styles.appContainer}
       direction={FlexBoxDirection.Column}
     >
       <Header id="app-header" />
-      
-      <FlexBox 
-        id="app-content" 
+
+      <FlexBox
+        id="app-content"
         className={styles.contentContainer}
-        direction={FlexBoxDirection.Column} 
+        direction={FlexBoxDirection.Column}
       >
         <Component {...pageProps} />
       </FlexBox>
-      
+
       <Footer id="app-footer" />
     </FlexBox>
   );
