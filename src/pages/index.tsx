49ad1d73772785f4594e1e1915e444622dc2f860
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-26 15:38:23 -0400

    Refactor UI layout for adding movie search bar and controls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, ThemeProvider } from '@ui5/webcomponents-react'
+import { Title, Text, ThemeProvider, FlexBox, FlexBoxDirection, Input, Button, ButtonType } from '@ui5/webcomponents-react'
 
 import styles from '../styles/index.module.scss'
 
@@ -15,6 +15,34 @@ export default function Home() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Laborum nobis itaque deserunt ipsa voluptate inventore minima voluptas voluptatibus tempora sed?
           </Text>
+
+          <form className={styles.movieSearchBar}>
+            <Input
+              id="movie-title-input" 
+              placeholder="Digite o título do filme"
+              className={styles.movieTitleInput}
+            />
+
+            <FlexBox 
+              id="button-container" 
+              direction={FlexBoxDirection.Row} 
+              className={styles.movieButtonContainer}
+            >
+              <Button 
+                id="search-button" 
+                className={styles.searchButton} 
+                type={ButtonType.Submit}
+                design="Emphasized" 
+              >Search</Button>
+              
+              <Button 
+                id="reset-button" 
+                className={styles.resetButton} 
+                type={ButtonType.Reset}
+                design="Transparent" 
+              >Reset</Button>
+            </FlexBox>
+          </form>
         </div>
       </div>
     </ThemeProvider>
