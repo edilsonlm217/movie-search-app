cb1559ea5b2a490971b75b01491c64b6fe448896
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-27 07:19:55 -0400

    Refactor home page layout and content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,68 @@
-import { Title, Text, ThemeProvider, FlexBox, FlexBoxDirection, Input, Button, ButtonType } from '@ui5/webcomponents-react'
-import MovieDetail from '../components/MovieDetail'
+import {
+  Title,
+  Text,
+  ThemeProvider,
+  FlexBox,
+  FlexBoxDirection,
+  Input,
+  Button,
+  ButtonType,
+} from "@ui5/webcomponents-react";
+import MovieDetail from "../components/MovieDetail";
 
-import styles from '../styles/index.module.scss'
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   return (
     <ThemeProvider>
       <div className={styles.pageContainer}>
         <div className={styles.content}>
-          <Title suppressHydrationWarning={true} id="page-title" level="H3" className={styles.pageTitle}>
-            Title
+          <Title
+            suppressHydrationWarning={true}
+            id="page-title"
+            level="H3"
+            className={styles.pageTitle}
+          >
+            Explore Movies
           </Title>
-          
+
           <Text id="page-description" className={styles.pageDescription}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Laborum nobis itaque deserunt ipsa voluptate inventore minima voluptas voluptatibus tempora sed?
+            Discover a world of captivating movies. Search for your favorites
+            and explore new releases.
           </Text>
 
           <form className={styles.movieSearchBar}>
             <Input
-              id="movie-title-input" 
-              placeholder="Digite o título do filme"
+              id="movie-title-input"
+              placeholder="Enter movie title"
               className={styles.movieTitleInput}
               suppressHydrationWarning={true}
             />
 
-            <FlexBox 
-              id="button-container" 
-              direction={FlexBoxDirection.Row} 
+            <FlexBox
+              id="button-container"
+              direction={FlexBoxDirection.Row}
               className={styles.movieButtonContainer}
             >
-              <Button 
-                id="search-button" 
-                className={styles.searchButton} 
+              <Button
+                id="search-button"
+                className={styles.searchButton}
                 type={ButtonType.Submit}
-                design="Emphasized" 
+                design="Emphasized"
                 suppressHydrationWarning={true}
-              >Search</Button>
-              
-              <Button 
-                id="reset-button" 
-                className={styles.resetButton} 
+              >
+                Search
+              </Button>
+
+              <Button
+                id="reset-button"
+                className={styles.resetButton}
                 type={ButtonType.Reset}
-                design="Transparent" 
+                design="Transparent"
                 suppressHydrationWarning={true}
-              >Reset</Button>
+              >
+                Reset
+              </Button>
             </FlexBox>
           </form>
 
@@ -52,5 +70,5 @@ export default function Home() {
         </div>
       </div>
     </ThemeProvider>
-  )
+  );
 }
