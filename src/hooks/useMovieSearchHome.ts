774c9a9aad068fad2458addfb9293763ab7db06a
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-27 19:38:37 -0400

    Refactor movie search hooks for improved separation of concerns

---
+++ src/hooks/useMovieSearchHome.ts
@@ -0,0 +1,44 @@
+import { useState, FormEvent } from "react";
+import { useMovieContext } from "../contexts/MovieContext";
+import { Ui5CustomEvent, InputDomRef } from "@ui5/webcomponents-react";
+
+interface UseMovieSearchHomeProps {
+  movieTitle: string;
+  handleInputChange: (event: Ui5CustomEvent<InputDomRef, never>) => void;
+  handleSearchClick: () => Promise<void>;
+  handleResetClick: () => void;
+  handleSubmit: (event: FormEvent) => Promise<void>;
+}
+
+const useMovieSearchHome = (): UseMovieSearchHomeProps => {
+  const [movieTitle, setMovieTitle] = useState("");
+  const { searchMovie, resetMovieData } = useMovieContext();
+
+  const handleInputChange = (event: Ui5CustomEvent<InputDomRef, never>) => {
+    setMovieTitle(event.target.value || "");
+  };
+
+  const handleSearchClick = async () => {
+    await searchMovie(movieTitle);
+  };
+
+  const handleResetClick = () => {
+    setMovieTitle("");
+    resetMovieData();
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    await searchMovie(movieTitle);
+  };
+
+  return {
+    movieTitle,
+    handleInputChange,
+    handleSearchClick,
+    handleResetClick,
+    handleSubmit,
+  };
+};
+
+export default useMovieSearchHome;
