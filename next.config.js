6a927e44549282671858a68b751aaae6f84e911d
Edilson R Lima <edilson.lima@meta.com.br>
2023-12-28 09:02:23 -0400

    Set NEXT_PUBLIC_BACKEND_URL environment variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: 'http://localhost:3001',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
